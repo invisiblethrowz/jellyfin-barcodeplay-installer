c9644a87d279ddd8ad35cf24bce5fc971624711d
invisiblethrowz <20bcapella@gmail.com>
2025-08-16 21:15:20 +1200

    Update PluginConfiguration.cs

--- PluginConfiguration.cs
+++ PluginConfiguration.cs
@@ -4,10 +4,7 @@ namespace Jellyfin.Plugin.BarcodePlay
 {
     public class PluginConfiguration : BasePluginConfiguration
     {
+        public string UPCItemDbApiKey { get; set; } = "demo-upc-key";
         public string TMDbApiKey { get; set; } = "demo-tmdb-key";
-        public string UPCItemDbApiKey { get; set; } = "demo-upcitemdb-key";
-        public string? DefaultSessionId { get; set; }
-        public bool PreferExactTitleMatch { get; set; } = true;
-        public bool HasSeenConfigOnce { get; set; } = false;
     }
 }
